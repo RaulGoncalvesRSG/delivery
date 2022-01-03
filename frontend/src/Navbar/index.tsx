76741c6ca0a9f1a229a22e3c129755e563af791f
Raul Gonçalves <raulsantos121@gmail.com>
2022-01-03 11:07:45 -0300

    Criação da NavBar

---
+++ frontend/src/Navbar/index.tsx
@@ -0,0 +1,14 @@
+import "./style.css";
+//ReactComponent é um nome genérico
+import {ReactComponent as Logo} from "./logo.svg";
+
+function NavBar(){
+    return (
+        <div className="main-navbar">
+            <Logo/>
+            <a href="home" className="logo-text">Delivery</a>
+        </div>
+    );
+}
+
+export default NavBar;
