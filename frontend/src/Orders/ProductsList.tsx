fa824a6e35b69faf14f12ac0ae3bfe0690f1f6e9
Raul Gonçalves <raulsantos121@gmail.com>
2022-01-05 09:21:25 -0300

    Seleção do pedido

--- frontend/src/Orders/ProductsList.tsx
+++ frontend/src/Orders/ProductsList.tsx
@@ -1,13 +1,16 @@
 import "./styles.css";
 import ProductCard from "./ProductCard";
 import { Product } from "./types";
+import {checkIsSelected} from "./helpers";
 
 //Props é um parãmetro passado pelo componente
 type Props = {
     products: Product[];
+    onSelectProduct: (product: Product) => void
+    selectedProducts: Product[];
 }
 
-function ProductsList({products}: Props){
+function ProductsList({products, selectedProducts, onSelectProduct}: Props){
     return (
         //Representa o bloco 100% da largura        
         <div className="orders-list-container">
@@ -15,7 +18,12 @@ function ProductsList({products}: Props){
             {/*Itens do produto */}
             <div className="orders-list-items">
                 {products.map(product => (
-                    <ProductCard key={product.id} product={product}/>
+                    <ProductCard 
+                        key={product.id} 
+                        product={product}
+                        onSelectProduct={onSelectProduct}
+                        isSelected={checkIsSelected(selectedProducts, product)}
+                    />
                 ))}
             </div>
         </div>
--- frontend/src/Orders/index.tsx
+++ frontend/src/Orders/index.tsx
@@ -2,13 +2,18 @@ import "./styles.css";
 import StepesHeader from "./StepesHeader";
 import ProductsList from "./ProductsList";
 import OrderLocation from "./OrderLocation";
+import OrderSummary from "./OrderSummary";
 import { useEffect, useState } from "react";
 import {OrderLocationData, Product} from "./types";
 import {fetchProducts} from "../api";
+import Footer from "../Footer";
+import {checkIsSelected} from "./helpers";
 
 function Orders(){
 
     const [products, setProducts] = useState<Product[]>([])
+    //Estado q guarda todos os produtos selecionados
+    const [selectedProducts, setSelectedProducts] = useState<Product[]>([])
     //Representa a localização selecionada pelo usuário
     const [orderLocation, setOrderLocation] = useState<OrderLocationData>()
 
@@ -19,13 +24,38 @@ function Orders(){
 
     }, []);
 
+    //handleSelectProduct é chamando qnd clica em algum item
+    const handleSelectProduct = (product: Product) => {
+        const isAlreadySelected = checkIsSelected(selectedProducts, product)
+        
+        //Já estiver selecionado, então desmarca o item
+        if (isAlreadySelected) {
+          //Filtra tds os selecionados com exceção daquele produto
+          const selected = selectedProducts.filter(item => item.id !== product.id);
+          setSelectedProducts(selected);
+        } 
+        else {
+            //Add o produto na lista
+          setSelectedProducts(previous => [...previous, product]);
+        }
+      }
+
     return (
-        <div className="orders-container">
-            <StepesHeader/>
-            <ProductsList products={products}/>
-            {/*Td vez q seleciona um endereço, emite um evento para o componente Order e armazena o endereço em setOrderLocation*/}
-            <OrderLocation onChangeLocation={location => setOrderLocation(location) }/>
-        </div>
+        <>
+            <div className="orders-container">
+                <StepesHeader/>
+                <ProductsList 
+                    //Td vez q seleciona um endereço, emite um evento para o componente Order e armazena o endereço em setOrderLocation
+                    products={products}
+                    onSelectProduct={handleSelectProduct}  
+                    selectedProducts={selectedProducts}  
+                />
+                <OrderLocation 
+                onChangeLocation={location => setOrderLocation(location) }/>
+                <OrderSummary/>
+            </div>
+            <Footer/>
+        </>
     );
 }
 
